740c5cc0bd7bbb130459bcd0610b2a2212848157
MarciiKai <kai.marcii2000@gmail.com>
2023-12-16 12:45:04 +0300

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,41 @@
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
-import { v4 as uuidv4 } from 'uuid';
-
-type Message = Record<{
-    id: string;
-    title: string;
-    body: string;
-    attachmentURL: string;
-    createdAt: nat64;
-    updatedAt: Opt<nat64>;
-}>
-
-type MessagePayload = Record<{
-    title: string;
-    body: string;
-    attachmentURL: string;
-}>
-
-const messageStorage = new StableBTreeMap<string, Message>(0, 44, 1024);
-
-$query;
-export function getMessages(): Result<Vec<Message>, string> {
-    return Result.Ok(messageStorage.values());
-}
-
-$query;
-export function getMessage(id: string): Result<Message, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => Result.Ok<Message, string>(message),
-        None: () => Result.Err<Message, string>(`a message with id=${id} not found`)
-    });
-}
-
-$update;
-export function addMessage(payload: MessagePayload): Result<Message, string> {
-    const message: Message = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, ...payload };
-    messageStorage.insert(message.id, message);
-    return Result.Ok(message);
-}
-
-$update;
-export function updateMessage(id: string, payload: MessagePayload): Result<Message, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => {
-            const updatedMessage: Message = {...message, ...payload, updatedAt: Opt.Some(ic.time())};
-            messageStorage.insert(message.id, updatedMessage);
-            return Result.Ok<Message, string>(updatedMessage);
-        },
-        None: () => Result.Err<Message, string>(`couldn't update a message with id=${id}. message not found`)
-    });
-}
-
-$update;
-export function deleteMessage(id: string): Result<Message, string> {
-    return match(messageStorage.remove(id), {
-        Some: (deletedMessage) => Result.Ok<Message, string>(deletedMessage),
-        None: () => Result.Err<Message, string>(`couldn't delete a message with id=${id}. message not found.`)
-    });
-}
-
-// a workaround to make uuid package work with Azle
-globalThis.crypto = {
-     // @ts-ignore
-    getRandomValues: () => {
-        let array = new Uint8Array(32);
-
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256);
-        }
-
-        return array;
+class NoPromptsAvailableError extends Error {
+    constructor(message: string) {
+      super(message);
+      this.name = "NoPromptsAvailableError";
     }
-};
+  }
+  
+  class ImaginationVortex {
+    private imaginativePrompts: string[];
+  
+    constructor() {
+      this.imaginativePrompts = [
+        "In a world where gravity is reversed...",
+        "A mysterious door appears in the middle of a forest...",
+        "The last person on Earth discovers a hidden library...",
+      ];
+    }
+  
+    getRandomPrompt(): string {
+      if (this.imaginativePrompts.length === 0) {
+        throw new NoPromptsAvailableError("No imaginative prompts available.");
+      }
+      const randomIndex = Math.floor(Math.random() * this.imaginativePrompts.length);
+      return this.imaginativePrompts[randomIndex];
+    }
+  }
+  
+  // Example usage:
+  const imaginationVortex = new ImaginationVortex();
+  
+  try {
+    const randomPrompt = imaginationVortex.getRandomPrompt();
+    console.log(`Imaginative Prompt: ${randomPrompt}`);
+  } catch (error) {
+    if (error instanceof NoPromptsAvailableError) {
+      console.log(`Error: ${error.message}`);
+    } else {
+      console.error(error);
+    }
+  }
+  
